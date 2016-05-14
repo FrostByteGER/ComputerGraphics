1897dbe33e0b18c1dc675f06fd7dcf919ab7b166
Jan Schult <janrippen@ymail.com>
2016-05-14 02:08:49 +0200

    nothing

--- LabEngine/collision.cpp
+++ LabEngine/collision.cpp
@@ -4,8 +4,6 @@ Collision::Collision(){
 }
     bool Collision::SphereVersusSphere(int x1 ,int y1 ,int z1 ,int size1 ,int x2 ,int y2 ,int z2 ,int size2){
 
-
-
         return     ((x1 - x2) * (x1 - x2) +
                     (y1 - y2) * (y1 - y2) +
                     (z1 - z2) * (z1 - z2)) >= ((size1+size2) * (size1+size2));
